0aa2628c05d79a28eb0bfd8ce8f3ba5b36df2789
yuxiao <zy060131@outlook.com>
2025-04-14 09:23:16 +0800

    Update

--- js/skystar.js
+++ js/skystar.js
@@ -1,72 +1,41 @@
-// poem
-var words=[
-    '伤心桥下春波绿',
-    '曾是惊鸿照影来',
-    '当年明月在',
-    '曾照彩云归',
-    '归去来兮',
-    '真堪偕隐',
-    '画船听雨眠',
-    '愿为江水',
-    '与君重逢',
-    '一日不见兮',
-    '思之若狂',
-    '好想回到那个夏天',
-    '趴在桌子上偷偷看你',
-    '偷偷表白一个叫dmz的女孩',
-    '你曾是我灰色人生中的一道彩虹',
-    '柳絮空缱绻',
-    '南风知不知',
-    '我见青山多妩媚',
-    '料青山见我也应如是',
-    '取次花丛懒回顾',
-    '半缘修道半缘君',
-    '三笑徒然当一痴',
-    '人生若只如初见',
-    '我余光中都是你',
-    '人生自是有情痴',
-    '此恨不关风与月',
-    '因为你，我多少适应了这个世界',
-    '春蚕到死丝方尽',
-    '蜡炬成灰泪始干',
-    '今夜何夕',
-    '见此良人',
-    '愿我如星君如月',
-    '夜夜流光相皎洁',
-    '情不所起',
-    '一往而深',
-    '玲珑骰子安红豆',
-    '入骨相思知不知',
-    '多情只有春庭月',
-    '尤为离人照落花',
-    '若有知音见采',
-    '不辞唱遍阳春',
-    '休言半纸无多重',
-    '万斛离愁尽耐担',
-    '夜月一帘幽梦',
-    '和光同尘',
-    '杳霭流玉',
-    '月落星沉',
-    '霞姿月韵',
-    '喜上眉梢',
-    '醉后不知天在水',
-    '满船星梦压星河',
-    '落花人独立',
-    '微雨燕双飞',
-    '掬水月在手',
-    '弄花香满衣',
-    '夜深忽梦少年事',
-    '唯梦闲人不梦君',
-    '垆边人似月',
-    '皓腕凝霜雪',
-    '众里嫣然通一顾',
-    '人间颜色如尘土',
-    '若非群玉山头见',
-    '会向瑶台月下逢',
-    '沉鱼落雁鸟惊喧',
-    '羞花闭月花愁颤',
-    '解释春风无限恨',
-    '沉香亭北倚阑干'
+//poems
+var words = [
+    '大不自多',
+    '海纳江河',
+    '惟学无际',
+    '际于天地',
+    '形上谓道兮',
+    '形下谓器',
+    '礼主别异兮',
+    '乐主和同',
+    '知其不二兮',
+    '尔听斯聪',
+    '国有成均',
+    '在浙之滨',
+    '昔言求是',
+    '实启尔求真',
+    '习坎示教',
+    '始见经纶',
+    '无曰已是',
+    '无曰遂真',
+    '靡革匪因',
+    '靡故匪新',
+    '何以新之',
+    '开物前民',
+    '嗟尔髦士',
+    '尚其有闻',
+    '念哉典学',
+    '思睿观通',
+    '有文有质',
+    '有农有工',
+    '兼总条贯',
+    '知至知终',
+    '成章乃达',
+    '若金之在熔',
+    '尚亨于野',
+    '无吝于宗',
+    '树我邦国',
+    '天下来同'
 ];
 function randomNum(min,max){
     var num = (Math.random()*(max-min+1)+min).toFixed(2);
@@ -99,23 +68,16 @@ function init(){
 window.addEventListener('load',init);
 let textone = document.querySelector('.textone').querySelector('h1');
       let texttwo = document.querySelector('.texttwo').querySelector('h1');
-      let textthree = document.querySelector('.textthree').querySelector('h1');
 
       setTimeout(function(){
-        textone.innerHTML = '今晚，整片星空将为你一人闪烁';
+        textone.innerHTML = '国有成均，在浙之滨';
           textone.style.color = '#E8F9FD';
           textone.style.fontFamily = '楷体'
           texttwo.style.color = '#E8F9FD';
           texttwo.style.fontFamily = '楷体'
-          textthree.style.color = '#E8F9FD';
-          textthree.style.fontFamily = '楷体'
           texttwo.innerHTML = '';
       },28000)
       setTimeout(function(){
-        textone.innerHTML = '从前从前,有个人爱你很久';
-        texttwo.innerHTML = '但偏偏，风渐渐';
-        textthree.innerHTML = '把距离吹的好远';
+        textone.innerHTML = '海纳江河，启真厚德';
+        texttwo.innerHTML = '开物前民，树我邦国';
       },112500)
-
-
- 
